a0eade139a40d3e632b0b7d3e17dae84eb8231f4
mdodge <dmikester1@gmail.com>
2020-01-24 14:49:46 -0600

    Add temp hi and low details to weather statement

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,13 +5,17 @@ const forecast = (latitude, longitude, callback) => {
     latitude + ',' + longitude;
 
   request({url, json: true}, (error, {body}) => {
+    console.log(body.daily.data[0]);
     if (error) {
       callback('Unable to connect to weather service!', undefined);
     } else if (body.error) {
       callback('Unable to find location', undefined);
     } else {
       callback(undefined, body.daily.data[0].summary +
-        ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.'
+        ' It is currently ' + body.currently.temperature + ' degrees out. The high is ' +
+        body.daily.data[0].temperatureHigh + ' with a low of ' +
+        body.daily.data[0].temperatureLow + '. There is a ' +
+        body.currently.precipProbability + '% chance of rain.'
       );
     }
   });
