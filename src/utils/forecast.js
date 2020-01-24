e9156b224c1a0b44d2a2b3890a8c97877f3afe5d
mdodge <mdodge@ecgrow.com>
2020-01-24 07:57:13 -0600

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url = 'https://api.darksky.net/forecast/ed6036dcd478792c81beb76240a01b5b/' +
+    latitude + ',' + longitude;
+
+  request({url, json: true}, (error, {body}) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(undefined, body.daily.data[0].summary +
+        ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
